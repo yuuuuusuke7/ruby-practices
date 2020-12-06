4db47383c527524f4785578025e457adf9a608fb
Yusuke <yusuke.motegi1119@gmail.com>
2020-12-07 04:48:49 +0900

    make_yusuke_wc.rb

---
+++ 06.wc/yusuke_wc.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+options = {}
+OptionParser.new do |opt|
+  opt.on('-l') { |l| options[:l] = l }
+  opt.parse!(ARGV)
+end
+
+def count_line(file_read)
+  file_read.count("\n")
+end
+
+def count_words(file_read)
+  file_read.split(/\s+/).size
+end
+
+def count_byte(file_read)
+  file_read.bytesize
+end
+
+def wc_command_items(file_name)
+  file_read = File.read(file_name)
+  wc_command_items = []
+  wc_command_items << count_line(file_read)
+  wc_command_items << count_words(file_read)
+  wc_command_items << count_byte(file_read)
+  wc_command_items
+end
+
+def print_wc_command(file_name)
+  wc_command_items(file_name).map { |wc_command_item| print wc_command_item.to_s.rjust(8) }
+  print " #{file_name} \n"
+end
+
+def wc_command_opt_l_items(file_name)
+  file_read = File.read(file_name)
+  wc_command_count_line = []
+  wc_command_count_line << count_line(file_read)
+  wc_command_count_line
+end
+
+def print_wc_command_opt_l(file_name)
+  wc_command_opt_l_items(file_name).map { |wc_command_opt_l_item| print wc_command_opt_l_item.to_s.rjust(8) }
+  print " #{file_name} \n"
+end
+
+def print_stdin
+  row_lines = $stdin.readlines
+  stdin_items = [row_lines.length, row_lines.to_s.split(/\s+/).size, row_lines.join.chars.size]
+  stdin_items.map { |stdin_item| print stdin_item.to_s.rjust(8) }
+  print "\n"
+end
+
+if ARGV.empty?
+  print_stdin
+elsif options[:l]
+  ARGV.map do |file_name|
+    print_wc_command_opt_l(file_name)
+  end
+else
+  ARGV.map do |file_name|
+    print_wc_command(file_name)
+  end
+end
+
+if ARGV.size > 1 && options[:l]
+  wc_command_opt_l_item_list = ARGV.map do |file_name|
+    wc_command_opt_l_items(file_name)
+  end
+  wc_command_opt_l_item_total = wc_command_opt_l_item_list.transpose.map { |count_line_file| count_line_file.inject(&:+) }
+  wc_command_opt_l_item_total.map { |transpose_count_line_file| print transpose_count_line_file.to_s.rjust(8) }
+  print " total \n"
+elsif ARGV.size > 1
+  wc_command_item_list = ARGV.map do |file_name|
+    wc_command_items(file_name)
+  end
+  wc_command_item_total = wc_command_item_list.transpose.map { |wc_command_item| wc_command_item.inject(&:+) }
+  wc_command_item_total.map { |same_wc_command_item| print same_wc_command_item.to_s.rjust(8) }
+  print " total \n"
+end
