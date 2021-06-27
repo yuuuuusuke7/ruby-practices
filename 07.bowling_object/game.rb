cfc7d71ffc11d172d4e4bf4307e0a5fc685385d3
Yusuke <yusuke.motegi1119@gmail.com>
2021-06-28 08:02:40 +0900

    Fix:variable names of game.rb

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -5,23 +5,23 @@ require './shot'
 
 class Game
   def initialize(input_text)
-    create_frame(input_text)
+    build_frames(input_text)
   end
 
   def calculate_score
-    [base_score, bonus_score].sum
+    [base_score_with_bonus_score_for_last_frame, bonus_score_without_bonus_score_of_last_frame].sum
   end
 
   private
 
-  def base_score
-    @frames.flatten.sum
+  def base_score_with_bonus_score_for_last_frame
+    @scores_for_each_frame.flatten.sum
   end
 
-  def bonus_score
+  def bonus_score_without_bonus_score_of_last_frame
     bonus = 0
-    @frame_instance.each_with_index do |shots, index|
-      following_pinfalls = @frames[index.succ..].flatten
+    @frames.each_with_index do |shots, index|
+      following_pinfalls = @scores_for_each_frame[index.succ..].flatten
       if shots.strike?
         bonus += following_pinfalls[0..1].sum
       elsif index < 9 && shots.spare?
@@ -31,17 +31,17 @@ class Game
     bonus
   end
 
-  def create_frame(pinfall_text)
-    @frames = []
+  def build_frames(pinfall_text)
+    @scores_for_each_frame = []
     frame = []
     Frame.pinfalls(pinfall_text).map do |shot|
       frame << shot
-      if frame.count == 2 || frame.count == 1 && shot == 10 || @frames[9]
-        @frames << frame
+      if frame.count == 2 || frame.count == 1 && shot == 10 || @scores_for_each_frame[9]
+        @scores_for_each_frame << frame
         frame = []
       end
     end
-    @frames = [*@frames[0..8], [*@frames[9], *@frames[10], *@frames[11]]]
-    @frame_instance = @frames.map { |shots| Frame.new(*shots) }
+    @scores_for_each_frame = [*@scores_for_each_frame[0..8], [*@scores_for_each_frame[9], *@scores_for_each_frame[10], *@scores_for_each_frame[11]]]
+    @frames = @scores_for_each_frame.map { |shots| Frame.new(*shots) }
   end
 end
