cbac148f15eb91ec352bd56bc3dd36e7b40d2c58
Yusuke <yusuke.motegi1119@gmail.com>
2021-07-01 08:15:25 +0900

    Merge: build_score_text method into build_frames

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -18,25 +18,4 @@ class Frame
   def spare?
     @first_shot != STRIKE && [@first_shot, @second_shot].sum == 10
   end
-
-  class << self
-    def build_score_text(pinfall_text)
-      shots = pinfall_text.split(',').map { |pinfall| Shot.new(pinfall) }
-
-      scores_for_each_frame = []
-      frame = []
-      shots.each do |shot|
-        frame << shot.score
-        if frame.count == 2 || frame.count == 1 && shot.score == 10 || scores_for_each_frame[9]
-          scores_for_each_frame << frame
-          frame = []
-        end
-      end
-      scores_for_each_frame = [*scores_for_each_frame[0..8], [*scores_for_each_frame[9], *scores_for_each_frame[10], *scores_for_each_frame[11]]]
-    end
-
-    def build_frames(pinfall_text)
-      @frames = build_score_text(pinfall_text).map { |shots| Frame.new(*shots) }
-    end
-  end
 end
--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -5,8 +5,7 @@ require './shot'
 
 class Game
   def initialize(input_text)
-    @frames = Frame.build_frames(input_text)
-    @score_text = Frame.build_score_text(input_text)
+    @frames = build_frames(input_text)
   end
 
   def calculate_score
@@ -16,13 +15,13 @@ class Game
   private
 
   def base_score_with_bonus_score_for_last_frame
-    @score_text.flatten.sum
+    @scores_for_each_frame.flatten.sum
   end
 
   def bonus_score_without_bonus_score_of_last_frame
     bonus = 0
     @frames.each_with_index do |shots, index|
-      following_pinfalls = @score_text[index.succ..].flatten
+      following_pinfalls = @scores_for_each_frame[index.succ..].flatten
       if shots.strike?
         bonus += following_pinfalls[0..1].sum
       elsif index < 9 && shots.spare?
@@ -30,5 +29,21 @@ class Game
       end
     end
     bonus
+  end
+
+  def build_frames(pinfall_text)
+    shots = pinfall_text.split(',').map { |pinfall| Shot.new(pinfall) }
+
+    scores_for_each_frame = []
+    frame = []
+    shots.each do |shot|
+      frame << shot.score
+      if frame.count == 2 || frame.count == 1 && shot.score == 10 || scores_for_each_frame[9]
+        scores_for_each_frame << frame
+        frame = []
+      end
+    end
+    @scores_for_each_frame = [*scores_for_each_frame[0..8], [*scores_for_each_frame[9], *scores_for_each_frame[10], *scores_for_each_frame[11]]]
+    @frames = @scores_for_each_frame.map { |shots| Frame.new(*shots) }
   end
 end
