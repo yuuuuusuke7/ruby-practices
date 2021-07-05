062ebb17fc963b9295345146ec09b1e7935a3925
Yusuke <yusuke.motegi1119@gmail.com>
2021-07-05 10:49:42 +0900

    create: ls_short_format

--- 08.ls_object/lib/ls.rb
+++ 08.ls_object/lib/ls.rb
@@ -2,15 +2,38 @@
 
 require 'etc'
 require 'pathname'
+MAX_FILENAME_COUNT = 21
 
 def run_ls(pathname, width: 80, long_format: false, reverse: false, dot_match: false)
-  'Hi'
+  filenames = Dir.glob('*').sort
+  ls_short_format(filenames.map(&:to_s), width: 80)
 end
 
 def ls_long_format(filenames)
   'Hi'
 end
 
+# MAX_FILENAME_COUNT = 21
 def ls_short_format(filenames, width: 80)
-  'Hi'
+  transposed_file_names = safe_transpose(filenames.each_slice(row_count(filenames, width: 80)).to_a)
+  format_table(transposed_file_names)
+end
+
+def columns(width: 80)
+  width / MAX_FILENAME_COUNT
+end
+
+def row_count(filenames, width: 80)
+  (filenames.count.to_f / columns(width: 80)).ceil
+end
+
+def safe_transpose(filenames)
+  first, *rest = filenames
+  first.zip(*rest)
+end
+
+def format_table(filenames)
+  filenames.map do |row_files|
+  row_files.map { |filename| filename.to_s.ljust(MAX_FILENAME_COUNT) }.join.rstrip
+  end.join("\n")
 end
